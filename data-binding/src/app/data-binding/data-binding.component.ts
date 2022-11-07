bed7a657f35f1f7934e0a4716eaf007ea3a7544e
Flávio Portugal <flavioportugal20@gmail.com>
2022-11-07 20:28:24 -0300

    Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,12 +11,35 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem: string = 'https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
 
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver:boolean = false;
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+    if(evento.key == 'Enter'){
+      this.valorSalvo = this.valorAtual;
+    }
+  }
+
+  salvarValor(valor: string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
